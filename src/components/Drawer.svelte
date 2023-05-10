36a2a29ed463ffa503876ed3c9a6e8c68200df34
TheWisePigeon <zozozozeph@gmail.com>
2023-05-10 06:45:30 +0000

    feat: add appbar

---
+++ src/components/Drawer.svelte
@@ -0,0 +1,9 @@
+<script></script>
+
+<div class="border-r-2 border-blue-500 p-2 w-[20%] h-[100%] flex flex-col gap-5">
+    <div>Test</div>
+    <div>Test</div>
+    <div>Test</div>
+    <div>Test</div>
+    
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
   import "../app.css";
+  import Appbar from "../components/Appbar.svelte";
+  import Drawer from "../components/Drawer.svelte";
 </script>
 
-<slot />
+<main class=" w-full h-screen " >
+    <Appbar/>
+    <div class="p-2 h-[93%] w-full flex">
+        <Drawer/>
+        <slot />
+    </div>
+</main>
