271a07f19e2da7e1c39bb303b6f85b21f5d528dd
TheWisePigeon <zozozozeph@gmail.com>
2023-05-11 22:13:20 +0000

    add reactive sidebar

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -1,6 +1,24 @@
-<div class="h-full w-full bg-slate-600 text-white flex flex-col items-center">
-   <div>
+<script lang="ts" >
+    import SidebarItem from "./SidebarItem.svelte";
+    const items : {title:string, path:string}[] = [
+        {
+                title:"Categories",
+                path:"/category"
+            },{
+                    title:"Sous categories",
+                    path:"/sub_category"
+                }
+    ]
+</script>
+
+<div class="h-full w-full bg-slate-600 text-white flex flex-col px-5">
+   <div class=" h-[10%] w-full ">
         <h1>LOGO</h1>
         <h1>GoodnessUnit</h1>
    </div> 
+   <div class=" gap-5 h-[90%] flex flex-col overflow-scroll no-bar">
+        {#each items as item}
+            <SidebarItem title={item.title} path={item.path} />
+        {/each}
+   </div>
 </div>
---
+++ src/components/SidebarItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" >
+    import { page } from "$app/stores"
+    export let title: string
+    export let path: string
+    $: active_tab = $page.url.pathname===path
+</script>
+
+<a href={path} class={`${active_tab?"bg-gray-500 text-blue-800":""}  text-2xl hover:text-blue-800 p-2 rounded-md font-bold `}>
+    <h1>{title}</h1>
+</a>
