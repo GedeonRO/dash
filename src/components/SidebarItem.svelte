d9980a52d1817500882b5af2ed6e2567a757a3f0
TheWisePigeon <zozozozeph@gmail.com>
2023-05-12 07:13:36 +0000

    add featured toggler

--- src/components/SidebarItem.svelte
+++ src/components/SidebarItem.svelte
@@ -5,6 +5,6 @@
     $: active_tab = $page.url.pathname===path
 </script>
 
-<a href={path} class={`${active_tab?"bg-gray-500 ":""}  text-2xl hover:text-blue-800 p-2 rounded-md font-bold `}>
+<a href={path} class={`${active_tab?"bg-gray-500 ":""}  text-2xl hover:bg-gray-500 p-2 rounded-md font-bold `}>
     <h1>{title}</h1>
 </a>
--- src/routes/(authed)/category/+page.svelte
+++ src/routes/(authed)/category/+page.svelte
@@ -1,55 +1,67 @@
 <script lang="ts">
-	import TrashIcon from "../../../components/TrashIcon.svelte";
-    import EditIcon from "../../../components/EditIcon.svelte";
-    const categories : {name:string, image:string, featured:boolean}[] = [
-        {
-                name:"Test",
-                image:"https://picsum.photos/200/300",
-                featured:false
-            },{
-                name:"Test",
-                image:"https://picsum.photos/200/300",
-                featured:false
-            },{
-                name:"Test",
-                image:"https://picsum.photos/200/300",
-                featured:false
-            },{
-                name:"Test",
-                image:"https://picsum.photos/200/300",
-                featured:false
-            },
-    ]
+	import TrashIcon from '../../../components/TrashIcon.svelte';
+	import EditIcon from '../../../components/EditIcon.svelte';
+    import Switch from '../../../components/Switch.svelte';
+	const categories: { name: string; image: string; featured: boolean }[] = [
+		{
+			name: 'Test',
+			image: 'https://picsum.photos/200/300',
+			featured: true
+		},
+		{
+			name: 'Test',
+			image: 'https://picsum.photos/200/300',
+			featured: false
+		},
+		{
+			name: 'Test',
+			image: 'https://picsum.photos/200/300',
+			featured: false
+		},
+		{
+			name: 'Test',
+			image: 'https://picsum.photos/200/300',
+			featured: false
+		}
+	];
 </script>
 
 <div class="w-full h-full p-2 flex flex-col gap-5">
 	<div class="flex justify-between w-full">
-        <input type="text" placeholder="Recherche" class="rounded-md p-2 w-96">
-        <button class="hover:bg-slate-500 rounded-md bg-slate-600 w-52 text-white" >Ajouter une categorie</button>
-    </div>
-    <div class="flex flex-col overflow-scroll no-bar w-full shadow-inner h-full">
-        <table class="table w-full border-spacing-5 " >
-            <tr class="w-full text-center font-bold">
-                <td>Nom</td>
-                <td>Image</td>
-                <td>Featured</td>
-                <td>Actions</td>
-            </tr>
-            {#each categories as category}
-               <tr class=" w-full text-xl text-center ">
-                    <td >{category.name}</td>
-                    <td class="flex justify-center" >
-                        <img class=" w-12 h-12 " alt="categorie" src={category.image} loading="lazy">
-                    </td>
-                    <td>{category.featured}</td>
-                    <td>
-                        <div class="w-full flex justify-center gap-5" >
-                            <EditIcon/>
-                            <TrashIcon/>
-                        </div>
-                    </td>
-               </tr> 
-            {/each}
-        </table>
-    </div>
+		<input type="text" placeholder="Recherche" class="rounded-md p-2 w-96" />
+		<button class="hover:bg-slate-500 rounded-md bg-slate-600 w-52 text-white"
+			>Ajouter une categorie</button
+		>
+	</div>
+	<div class="flex flex-col overflow-scroll no-bar w-full shadow-inner h-full">
+		<table class="table-auto">
+			<thead class="border-b-2 border-slate-300">
+				<tr class="w-full font-bold">
+					<td>Nom</td>
+					<td>Image</td>
+					<td>Featured</td>
+					<td>Actions</td>
+				</tr>
+			</thead>
+			<tbody>
+				{#each categories as category}
+					<tr class=" w-full text-xl border-b-2 border-slate-300">
+						<td class="py-5" >{category.name}</td>
+						<td class="">
+							<img class=" w-12 h-12" alt="categorie" src={category.image} loading="lazy" />
+						</td>
+						<td>
+                            <Switch toggled={category.featured} />
+                        </td>
+						<td>
+							<div class="flex gap-5">
+								<EditIcon />
+								<TrashIcon />
+							</div>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 </div>
