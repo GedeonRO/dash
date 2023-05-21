ad86b6c6013064d26f01fe88b988cdf2d0a7e615
TheWisePigeon <zozozozeph@gmail.com>
2023-05-21 13:25:56 +0000

    feat: logout

---
+++ src/routes/(authed)/logout/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" >
+	import { browser } from "$app/environment";
+	import { onMount } from "svelte";
+
+    onMount(()=>{
+            if(browser){
+                    localStorage.removeItem("token")
+                    window.location.href="/auth"
+                }
+        })
+</script>
